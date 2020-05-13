63aa9ec32fe56dc79a3da05a5874630c007c3afe
ohleenda <lindarivero@ymail.com>
2020-05-12 21:26:30 -0700

    step 2

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
-function numberGame(x, y) {
-	var numArray = [];
-	if (x > y) {
-		for (i=y+1; i<x; i++) {
-			if (i%2 == 0) {
-				numArray.push(i);
-			}
-		}
-	} else {
-		for (i=x+1; i<y; i++) {
-			if (i%2 != 0) {
-				numArray.push(i);
-			}
-		}
-	}
-	return numArray;
-}
+function evenNum() {
+    var n1 = parseInt(document.getElementById("n1").value);
+    var n2 = parseInt(document.getElementById("n2").value);
+  
+    if (n1 <= 2 || n1 >= 100) {
+      console.log(parseInt(n1, 10));
+      document.getElementById("msg").innerHTML +=
+        "Number 1 input" + n1 + " is not a valid number.";
+      document.getElementById("result").innerHTML = "";
+    }
+    if (n2 <= 2 || n2 >= 100) {
+      document.getElementById("msg").innerHTML +=
+        "Number 1 input" + n2 + " is not a valid number.";
+      document.getElementById("result").innerHTML = "";
+    } else if (n1 >= 2 && n1 <= 100 && n2 >= 2 && n2 <= 100) {
+      calculateEvenNum(n1, n2);
+    }
+  }
+
+  function calculateEvenNum(firstNum, secondNum) {
+    var eNumArray = [];
+    var sNum = 0;
+    var bNum = 0;
+  
+    if (firstNum > secondNum) {
+      sNum = secondNum;
+      bNum = firstNum;
+    } else if (firstNum < secondNum) {
+      sNum = firstNum;
+      bNum = secondNum;
+    }
+  
+    while (sNum <= bNum) {
+      if (isEven(sNum) == true) {
+        eNumArray.push(sNum);
+        sNum = sNum + 1;
+      } else if (isEven(sNum) == false) {
+        sNum = sNum + 1;
+      }
+    }
+
+    console.log(eNumArray);
+    document.getElementById("result").innerHTML =
+      "There are " + eNumArray.length + " even numbers:";
+    document.getElementById("msg").innerHTML = eNumArray;
+  }
+
+  function isEven(num) {
+    res = true;
+    for (var i = num; i <= num + 1; i++) {
+      if (num % 2 == 0) {
+        res = true;
+      } else {
+        res = false;
+      }
+      return res;
+    }
+  }
